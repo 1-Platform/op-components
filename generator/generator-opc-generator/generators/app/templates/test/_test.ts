8643605cd66e23ae9810a54b277d3c5f9efde289
Sumeet Ingole <singole@redhat.com>
2020-09-24 14:02:00 +0530

    Adds support for accessibility test.

--- generator/generator-opc-generator/generators/app/templates/test/_test.ts
+++ generator/generator-opc-generator/generators/app/templates/test/_test.ts
@@ -1,31 +1,33 @@
-import {LitElement} from 'lit-element';
+import { axe, toHaveNoViolations } from 'jest-axe';
+import { <%= componentClass %> } from '../src/<%= componentName %>';
+
+expect.extend(toHaveNoViolations);
 
 describe('<%= componentName %>', () => {
 
     const OPC_COMPONENT = '<%= componentName %>';
     const ELEMENT_ID = '<%= componentName %>';
-    let buttonElement: LitElement;
+    let opcElement: <%= componentClass %>;
 
     const getShadowRoot = (tagName: string): ShadowRoot => {
         return document.body.getElementsByTagName(tagName)[0].shadowRoot;
     }
 
     beforeEach(() => {
-        buttonElement = window.document.createElement(OPC_COMPONENT) as LitElement;
-        document.body.appendChild(buttonElement);
+        opcElement = window.document.createElement(OPC_COMPONENT) as <%= componentClass %>;
+        document.body.appendChild(opcElement);
     });
 
     afterEach(() => {
        document.body.getElementsByTagName(OPC_COMPONENT)[0].remove();
     });
 
-    it('displays button text', async () => {
-        buttonElement.setAttribute('name', '<%= componentName %>');
-        await buttonElement.updateComplete;
+    it('is defined', async () => {
+        expect(opcElement).toBeDefined();
+    });
 
-        const renderedText = getShadowRoot(OPC_COMPONENT).getElementById(ELEMENT_ID).innerText;
-
-        expect(renderedText).toEqual('<%= componentName %>');
+    it('has no axe violations', async () => {
+        expect(await axe(opcElement)).toHaveNoViolations()
     });
 
     // Add more tests here
