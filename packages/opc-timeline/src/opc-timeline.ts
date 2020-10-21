a53ed81a4b5b3360f465b6985f0d7ff588b1e587
hybridx <hybridx18@gmail.com>
2020-10-22 03:30:08 +0530

    fix(#94) - Added support for scroll

--- packages/opc-timeline/src/opc-timeline.ts
+++ packages/opc-timeline/src/opc-timeline.ts
@@ -1,12 +1,50 @@
-import { LitElement, html, property, customElement } from 'lit-element';
+import { LitElement, html, property, customElement, css } from 'lit-element';
 import style  from './opc-timeline.scss';
 
 @customElement('opc-timeline')
 export class Timeline extends LitElement {
   @property({ type: Array, attribute: 'steps'}) steps = [];
   @property({ type: Number, attribute: 'current-step-index' }) currentStepIndex = 0;
+  @property({ type: String, attribute: 'variant'}) variant = 'default';
   static get styles() {
     return [ style ];
+  }
+
+  scrollToLeft() {
+    this.shadowRoot.querySelector('#timeline-steps').scrollBy({
+      left: -900,
+      behavior: 'smooth'
+    });
+  }
+
+  scrollToRight() {
+    this.shadowRoot.querySelector('#timeline-steps').scrollBy({
+      left: 900,
+      behavior: 'smooth'
+    });
+  }
+
+  getArrow() {
+    if (this.variant === 'compact') {
+      return {
+        left: html`
+          <span class="timeline__arrow left" @click="${this.scrollToLeft}">
+            <div class="arrow">
+            </div>
+          </span>`,
+          right: html`
+          <span class="timeline__arrow right" @click="${this.scrollToRight}">
+            <div class="arrow">
+            </div>
+          </span>
+          `,
+      }
+    } else {
+      return {
+        left: '',
+        right: ''
+      }
+    }
   }
 
   _eventEmitter(stepIndex: number, stepItem: string|null) {
@@ -27,24 +65,39 @@ export class Timeline extends LitElement {
 
   render() {
     return html`
-      <ul class="timeline">
-        ${this.steps.map((step, index) => {
-          if (step) {
-            return html`
-            <li 
-              class="timeline__step ${this.currentStepIndex === index ? 'active' : ''}"
-              @click="${() => {this._eventEmitter(index, step)}}">
-                ${step}
-            </li>`;
-          } else {
-            return html`
-              <li 
-                class="timeline__step ${this.currentStepIndex === index ? 'active' : ''}"
-                @click="${() => {this._eventEmitter(index, null)}}">
+    <style>
+      .compact .timeline-steps {
+        overflow: hidden;
+      }
+      .compact .timeline-steps::before {
+        width: ${this.steps.length * 180}px !important;
+      }
+      .compact .timeline-steps .timeline-steps__step {
+        flex: 0 0 180px;
+      }
+    </style>
+    <div class="timeline ${this.variant === 'compact' ? 'compact' : ''}">
+        ${this.getArrow().left}
+        <ul id="timeline-steps" class="timeline-steps">
+          ${this.steps.map((step, index) => {
+            if (step) {
+              return html`
+              <li
+                class="timeline-steps__step ${this.currentStepIndex === index ? 'active' : ''}"
+                @click="${() => {this._eventEmitter(index, step)}}">
+                  ${step}
+              </li>`;
+            } else {
+              return html`
+                <li 
+                  class="timeline-steps__step ${this.currentStepIndex === index ? 'active' : ''}"
+                  @click="${() => {this._eventEmitter(index, null)}}">
                 </li>`;
-          }
-        })}
-      </ul>
+            }
+          })}
+        </ul>
+        ${this.getArrow().right}
+      </div>
       <div class="timeline-label">
         <slot name="start-label"></slot>
         <slot name="end-label"></slot>
