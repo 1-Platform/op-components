43ce01ee89e64ce5511546dfa6f3dad104dd2808
hybridx <hybridx18@gmail.com>
2020-12-18 17:33:41 +0530

    fix(#163) - Added webpack serve in all components

--- packages/opc-footer/webpack.config.js
+++ packages/opc-footer/webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
+        open: true,
         port: 4200,
     },
     entry: `${path.join(__dirname, 'src/opc-footer')}`,
