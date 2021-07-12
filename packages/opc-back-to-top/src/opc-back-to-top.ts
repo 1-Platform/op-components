3e5c9850ff60a1a5c2d3556772663bcdffbc94ec
Sumeet Ingole <gsumitingole@gmail.com>
2021-07-12 18:13:29 +0530

    updated dev dependency for opc-back-to-top.

--- packages/opc-back-to-top/src/opc-back-to-top.ts
+++ packages/opc-back-to-top/src/opc-back-to-top.ts
@@ -1,4 +1,5 @@
-import { LitElement, html, property, customElement } from 'lit-element';
+import { LitElement, html } from 'lit';
+import { property, customElement} from 'lit-element';
 import style from './opc-back-to-top.scss';
 
 @customElement('opc-back-to-top')
