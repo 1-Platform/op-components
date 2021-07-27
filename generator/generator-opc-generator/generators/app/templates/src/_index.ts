cb8645c4f873eb59f1bf9c8679da566cffb5461c
Deepesh Nair <hybridx18@gmail.com>
2021-07-27 21:54:36 +0530

    fix(#355) - Updated the generator code to lit.20 (#356)

--- generator/generator-opc-generator/generators/app/templates/src/_index.ts
+++ generator/generator-opc-generator/generators/app/templates/src/_index.ts
@@ -1,4 +1,5 @@
-import { LitElement, html, property, customElement } from 'lit-element';
+import {LitElement, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
 import style  from './<%= componentName %>.scss';
 
 @customElement('<%= componentName %>')
