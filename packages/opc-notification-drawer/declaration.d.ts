b74f2c517d7ce38eebf6a130c79e20fb2b5e85a2
akhilmhdh <akhilmhdh@gmail.com>
2021-07-30 17:49:38 +0530

    feat(1332): completed opc-notification-drawer

--- packages/opc-notification-drawer/declaration.d.ts
+++ packages/opc-notification-drawer/declaration.d.ts
@@ -1,2 +1,3 @@
 declare module '*.scss';
 // Add your type definitions here
+type Variants = 'success' | 'warn' | 'info' | 'danger';
--- packages/opc-notification-drawer/demo/opc-notification-drawer.stories.js
+++ packages/opc-notification-drawer/demo/opc-notification-drawer.stories.js
@@ -2,14 +2,36 @@ import '../dist/opc-notification-drawer';
 import readme from '../README.md';
 
 export default {
-    title: 'OpcNotificationDrawer',
-    parameters: {
-        notes: { readme },
-    }
-}
+  title: 'Opc Notification Drawer',
+  parameters: {
+    notes: { readme },
+  },
+  argTypes: {
+    isOpen: {
+      control: 'boolean',
+      description: 'To open the drawer',
+    },
+    title: {
+      control: 'string',
+      description: 'Title of the drawer',
+      defaultValue: 'notifications',
+    },
+  },
+};
 
-export const Primary = () => `
-<opc-notification-drawer></opc-notification-drawer>
+export const OpcNotificationDrawer = () => `
+<opc-notification-drawer isOpen>
+    <opc-notification-item title="Success alert title" variant="success" key="notification1">
+        <span>Success alert description. This should tell the user more information about the alert. This is a link.</span>
+    </opc-notification-item>
+    <opc-notification-item title="Information alert title" variant="info">
+        <span>Information alert description. This should tell the user more information about the alert.</span>
+    </opc-notification-item>
+    <opc-notification-item title="Danger alert title" variant="danger">
+        <span>Danger alert description. This should tell the user more information about the alert. This is a link.</span>
+    </opc-notification-item>
+    <opc-notification-item title="Warn alert title" variant="warn">
+        <span>Warn alert description. This should tell the user more information about the alert. This is a link.</span>
+    </opc-notification-item>
+</opc-notification-drawer>
 `;
-
-Primary.storyName = `OpcNotificationDrawer`;
